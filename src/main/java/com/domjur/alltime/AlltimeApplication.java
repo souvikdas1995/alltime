97ad50c23907cb3d9925b680569737c9eb649206
aditidas45 <svkds1995@gmail.com>
2024-07-03 19:43:34 +0530

    Auth service created and tested with Keycloak

---
+++ src/main/java/com/domjur/alltime/AlltimeApplication.java
@@ -0,0 +1,13 @@
+package com.domjur.alltime;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AlltimeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AlltimeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/domjur/alltime/AlltimeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.domjur.alltime;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AlltimeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
